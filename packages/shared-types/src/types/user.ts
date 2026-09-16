47dc88446165eeffa823f374a48c03526aa73f2e
Hairum Qureshi <hairum.qqureshi@gmail.com>
2026-09-15 23:50:02 -0400

    fixed UserPayload type properties

--- packages/shared-types/src/types/user.ts
+++ packages/shared-types/src/types/user.ts
@@ -1,10 +1,11 @@
 type UserPayload = {
-	_id: string;
+	id: string;
 	firstName: string;
 	lastName: string;
 	email: string;
-	profilePicture?: string;
+	profilePicture: string;
 	createdAt: Date;
+	updatedAt: Date;
 };
 
 export type { UserPayload };
